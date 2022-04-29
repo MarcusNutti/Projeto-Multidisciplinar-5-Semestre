06f5f121a135382b8232cb820601f587a7b440bc
Maieru Joao <soljoaomaieru@hotmail.com>
2022-04-29 11:54:43 -0300

    Adição da biblioteca Sweet Alert e de função js genérica para confirmação de exclusão de registros

--- Web Application/wwwroot/js/site.js
+++ Web Application/wwwroot/js/site.js
@@ -1,4 +1,17 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿function DeleteConfirmation(controllerName, objectID) {
+    Swal.fire({
+        showCancelButton: true,
+        cancelButtonText: `Não apagar`,
+        title: "Você Tem Certeza?",
+        text: "Uma vez deletada, você não conseguirá recuperar esse registro!",
+        icon: 'warning',
+    })
+        .then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire({
+                    text: "Deletado com Sucesso",
+                    icon: "success",
+                }).then(() => location.href = `${controllerName}/Delete?Id=${objectID}`);
+            }
+        });
+}
