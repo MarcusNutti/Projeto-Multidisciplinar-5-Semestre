c509562a4ee70b696827f61d1afd9b3650b1eaee
Maieru Joao <soljoaomaieru@hotmail.com>
2022-05-14 17:13:48 -0300

    .

--- Web Application Project/Controllers/BairroController.cs
+++ Web Application Project/Controllers/BairroController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Net.Http;
 using System.Reflection;
 using Web_Application.DAO;
 using Web_Application.Enum;
@@ -10,13 +11,13 @@ namespace Web_Application.Controllers
 {
     public class BairroController : GenericController<BairroViewModel>
     {
+
         protected override void SetAutenticationRequirements()
         {
             AutenticationRequired = true;
             MinumumLevelRequired = EnumTipoUsuario.Tecnico;
         }
         protected override void SetIdGenerationConfig() => GeraProximoId = true;
-
         protected override void SetDAO() => DAO = new BairroDAO();
 
         [HttpGet("api/SearchBairro")]
@@ -35,6 +36,28 @@ namespace Web_Application.Controllers
                                        action: MethodInfo.GetCurrentMethod()?.Name);
 
                 return View("Error", new ErrorViewModel(erro.ToString()));
+            }
+        }
+
+        [HttpGet("api/GetLatLong")]
+        public IActionResult GetLatLong(string CEP)
+        {
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    var url = $"https://maps.googleapis.com/maps/api/geocode/json?address={CEP}&key={APIKeys.GOOGLE_API_KEY}";
+
+                    var response = httpClient.GetAsync
+                }
+            }
+            catch (Exception erro)
+            {
+                LogService.GeraLogErro(erro,
+                                       controller: GetType().Name,
+                                       action: MethodInfo.GetCurrentMethod()?.Name);
+
+                return null;
             }
         }
     }
