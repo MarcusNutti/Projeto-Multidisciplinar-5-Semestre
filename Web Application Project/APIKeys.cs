04e6e675674f9437286193732346387b1280965d
Maieru <soljoaomaieru@hotmail.com>
2022-05-14 17:25:14 -0300

    Revert "Modificação no GitIgnore"
    This reverts commit 85d85dec648bd6870d73cfd81fc790a9f163f5dc.

--- Web Application Project/APIKeys.cs
+++ Web Application Project/APIKeys.cs
@@ -2,6 +2,6 @@
 {
     public static class APIKeys
     {
-        public const string GOOGLE_API_KEY = "AIzaSyB9v8nEM_QT4eIRbfcyDHz4nlWdnVns3Fs";
+        public const string GOOGLE_API_KEY = "";
     }
 }
