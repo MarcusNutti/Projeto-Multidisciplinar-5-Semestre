85d85dec648bd6870d73cfd81fc790a9f163f5dc
Maieru Joao <soljoaomaieru@hotmail.com>
2022-05-14 17:10:17 -0300

    Modificação no GitIgnore

--- Web Application Project/APIKeys.cs
+++ Web Application Project/APIKeys.cs
@@ -2,6 +2,6 @@
 {
     public static class APIKeys
     {
-        public const string GOOGLE_API_KEY = "";
+        public const string GOOGLE_API_KEY = "AIzaSyB9v8nEM_QT4eIRbfcyDHz4nlWdnVns3Fs";
     }
 }
