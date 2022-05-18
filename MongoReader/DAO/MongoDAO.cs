b215b7804a672ba068602c4374b34add890df477
JOAO VICTOR MAIERU TEIXEIRA <081200025@faculdade.cefsa.edu.br>
2022-05-18 20:22:08 -0300

    Adicionado sistema de leitura do mongo DB

---
+++ MongoReader/DAO/MongoDAO.cs
@@ -0,0 +1,48 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MongoReader.DAO
+{
+    public class MedicaoMongoObject
+    {
+        public ObjectId _id { get; set; }
+        public double idDispositivo { get; set; }
+        public string data { get; set; }
+        public double valorAgua { get; set; }
+        public double valorNivel { get; set; }
+    }
+
+    public static class MongoDAO
+    {
+        private static IMongoCollection<BsonDocument> GetTabelaSensores()
+        {
+            string connectionString = "mongodb://127.0.0.1:27017";
+            var client = new MongoClient(connectionString);
+            var banco = client.GetDatabase("HelixDatabase");
+            return banco.GetCollection<BsonDocument>("sensores");
+        }
+
+        public static List<MedicaoMongoObject> GetMedicoesUltimoMinuto()
+        {
+            var tabela = GetTabelaSensores();
+
+            // Alterar o .ToString para o formato gerado no Helix 
+            string horarioUltimoMinutoFormatado = DateTime.Now.AddMinutes(-1).ToString("dd/MM/yyyyTHH:mm:ss");
+
+            var filtro = Builders<BsonDocument>.Filter.Gte("data", horarioUltimoMinutoFormatado);
+
+            var registros = tabela.Find(filtro);
+
+            var listaRetorno = new List<MedicaoMongoObject>();
+
+            foreach (var registro in registros.ToList())
+                listaRetorno.Add(BsonSerializer.Deserialize<MedicaoMongoObject>(registro));
+
+            return listaRetorno;
+        }
+    }
+}
--- MongoReader/Program.cs
+++ MongoReader/Program.cs
@@ -1,48 +1,58 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Driver;
+using MongoReader.DAO;
+using MongoReader.Models;
 using System;
 using System.Collections.Generic;
 using System.Text.Json;
+using System.Threading;
+using MongoReader.DAO;
+using System.Globalization;
 
 namespace MongoReader
 {
     internal class Program
     {
-        class Mock
-        {
-            public DateTime Data { get; set; }
-            public double ValorAgua { get; set; }
-            public double NivelAgua { get; set; }
-        }
+        /*use HelixDatabase
 
+          db.sensores.insert({ data: "17/05/2022", valorAgua: 10, valorNivel: 20, idDispositivo: 1 })
+          db.sensores.insert({ data: "15/05/2022", valorAgua: 15, valorNivel: 28, idDispositivo: 1 })
+         */
         static void Main(string[] args)
         {
-            /*use HelixDatabase
-
-              db.sensores.insert({ data: "17/05/2022", valorAgua: 10, valorNivel: 20 })
-              db.sensores.insert({ data: "15/05/2022", valorAgua: 15, valorNivel: 28 })
-             */
-
-            MongoClient dbClient = new MongoClient("mongodb://127.0.0.1:27017");
-            var dbList = dbClient.ListDatabases().ToList();
-
-            var database = dbClient.GetDatabase("HelixDatabase");
-
-            var tabela = database.GetCollection<BsonDocument>("sensores");
-
-            var filtro = Builders<BsonDocument>.Filter.Eq("data", "17/05/2022");
-
-            var registros = tabela.Find(filtro); // Esses registros estão no formato retardado (BSON, BSON)
-
-            var teste = new List<Mock>();
-
-            foreach(var bisao in registros.ToList())
+            while (true)
             {
-                teste.Add(JsonSerializer.Deserialize<Mock>(bisao.ToString()));
-            }
+                try
+                {
+                    var ultimasMedicoes = MongoDAO.GetMedicoesUltimoMinuto();
+                    var medicaoDAO = new MedicaoDAO();
 
-            Console.WriteLine("Hello World!");
+                    foreach(var medicaoMongo in ultimasMedicoes)
+                    {
+                        var medicaoViewModel = MedicaoMongoToMedicaoViewModel(medicaoMongo);
+
+                        medicaoDAO.Insert(medicaoViewModel);
+                    }
+                }
+                catch (Exception erro)
+                {
+
+                }
+
+                Thread.Sleep(60000);
+            }
+        }
+
+        static MedicaoViewModel MedicaoMongoToMedicaoViewModel(MedicaoMongoObject medicaoMongo)
+        {
+            return new MedicaoViewModel
+            {
+                DataMedicao = DateTime.ParseExact(medicaoMongo.data, "dd/MM/yyyyTHH:mm:ss", CultureInfo.InvariantCulture),
+                DispositivoId = Convert.ToInt32(medicaoMongo.idDispositivo),
+                ValorChuva = Convert.ToDouble(medicaoMongo.valorAgua),
+                ValorNivel = Convert.ToDouble(medicaoMongo.valorNivel)
+            };
         }
     }
 }
